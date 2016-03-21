1fb7385e38f9cb8ca43eaa9facdfa411e21d8875
dinardo <dinardo@0e100f5f-f436-4db1-9290-6bc742a23f36>
2016-03-21 09:54:02 +0000

    Minor
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/monicelli/trunk@61 0e100f5f-f436-4db1-9290-6bc742a23f36

--- include/clusterizer.h
+++ include/clusterizer.h
@@ -38,8 +38,6 @@
 #include <string>
 #include <vector>
 
-//#include "process.h"
-
 #include "Event.h"
 #include "Geometry.h"
 #include "EventHeader.h"
@@ -47,42 +45,41 @@
 
 class clusterizer : public subProcess<Event,Geometry>
 {
-
-public:
-    clusterizer(void);
-    ~clusterizer(void);
-
-    void                       clear           (void                                    );
-    //  void                       execute         (void                                    );
-    Event::clustersHitsMapDef  findClusters    (Event * theEvent                        );
-    Event::clustersMapDef      makeClusters    (Event * theEvent, Geometry * theGeometry);
-    void                       clusterize      (Event * theEvent, Geometry * theGeometry);
-    void                       execute         (Event * theEvent, Geometry * theGeometry);
-    void                       setClustersBuilt(int     built                           ) {clustersBuilt_ = built;}
-    bool                       clustersBuilt   (void                                    ) {return clustersBuilt_ ;}
-    std::string                getLabel        (void                                    ) ;
-    std::string                getName         (void                                    ) {return "clusterizer"        ;}
-    void                       setHeader       (EventHeader * theHeader                 ) {theHeader->setClustersDone(clustersBuilt_);}
-    void                       getChargeAsymmetryPlots(Geometry * theGeometry);
-    void                       setUseEtaFunction(bool use                               ){useEtaFunction_ = use;}
-
-private:
-
-    std::string getPlaneID (int station, int plaquette);
-
-    //        station             ROCID           X|Y       pars[]
-    std::map<std::string,std::map<int,std::map<std::string,double* > > >chargeAsymmetryPars_;
-
-    Event::clustersHitsMapDef clustersHitsMap_     ;
-    Event::clustersMapDef     clustersMap_         ;
-
-    Event*                    theEvent_            ;
-    Geometry*                 theGeometry_         ;
-
-    bool                      clustersBuilt_       ;
-    bool                      useEtaFunction_      ;
-
-    std::stringstream         ss_                  ;
+  
+ public:
+  clusterizer(void);
+  ~clusterizer(void);
+  
+  void                       clear           (void                                    );
+  Event::clustersHitsMapDef  findClusters    (Event * theEvent                        );
+  Event::clustersMapDef      makeClusters    (Event * theEvent, Geometry * theGeometry);
+  void                       clusterize      (Event * theEvent, Geometry * theGeometry);
+  void                       execute         (Event * theEvent, Geometry * theGeometry);
+  void                       setClustersBuilt(int     built                           ) {clustersBuilt_ = built;}
+  bool                       clustersBuilt   (void                                    ) {return clustersBuilt_ ;}
+  std::string                getLabel        (void                                    ) ;
+  std::string                getName         (void                                    ) {return "clusterizer"        ;}
+  void                       setHeader       (EventHeader * theHeader                 ) {theHeader->setClustersDone(clustersBuilt_);}
+  void                       getChargeAsymmetryPlots(Geometry * theGeometry);
+  void                       setUseEtaFunction(bool use                               ) {useEtaFunction_ = use;}
+  
+ private:
+  
+  std::string getPlaneID (int station, int plaquette);
+  
+  //        station             ROCID           X|Y       pars[]
+  std::map<std::string,std::map<int,std::map<std::string,double* > > >chargeAsymmetryPars_;
+  
+  Event::clustersHitsMapDef clustersHitsMap_     ;
+  Event::clustersMapDef     clustersMap_         ;
+  
+  Event*                    theEvent_            ;
+  Geometry*                 theGeometry_         ;
+  
+  bool                      clustersBuilt_       ;
+  bool                      useEtaFunction_      ;
+  
+  std::stringstream         ss_                  ;
 };
 
 #endif // CLUSTERIZER_H
