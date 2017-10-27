389f6ae61f4fa3a86a1a4700072ebaeeae226439
Dario Menasce <dario.menasce@mib.infn.it>
2017-10-27 09:41:55 -0500

    Fixed a stupid bug in the settings manager and placed colors in purgeConfigs.pl

--- perlScripts/purgeConfigs.pl
+++ perlScripts/purgeConfigs.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+&defineColors() ;
+
 my $conf = $ENV{HOME} . "/.config/CMS";
 my $cmd  = "ls ${conf}/MonicelliDefaults*.conf" ;
 my @list = `$cmd` ;
@@ -35,10 +37,41 @@ foreach my $n1 (0 .. scalar @list - 1)
    {   
     unless( $c2 =~ m/MonicelliDefaults.conf/) 
     {
-     print(__LINE__ . "] [purgeConfigs.pl] removing $c2, an identical older one exists\n" ) ;
+     print("${ACCyan}${ACBold}" . __LINE__ . "      ${ACYellow}] [${ACWhite}purgeConfigs.pl                  ${ACYellow}]${ACPlain}   \n") ;
+     print("${ACCyan}${ACBold}" . __LINE__ . "      ${ACYellow}] [${ACWhite}purgeConfigs.pl                  ${ACYellow}]   ${ACRed}${ACBold}Removing ${ACCyan}$c2${ACRed}: an identical older one exists${ACPlain}\n") ;
+     print("${ACCyan}${ACBold}" . __LINE__ . "      ${ACYellow}] [${ACWhite}purgeConfigs.pl                  ${ACYellow}]${ACPlain}   \n") ;
      system("rm $c2") ;
     }
    }
   }
  }
+}
+#======================================================
+sub defineColors
+{
+  $ACBlack	 = "\x1B[0;30m";
+  $ACBlue	 = "\x1B[0;34m";
+  $ACGreen	 = "\x1B[0;32m";
+  $ACCyan	 = "\x1B[0;36m";
+  $ACRed	 = "\x1B[0;31m";
+  $ACPurple	 = "\x1B[0;35m";
+  $ACBrown	 = "\x1B[0;33m";
+  $ACGray	 = "\x1B[0;37m";
+  $ACDarkGray	 = "\x1B[1;30m";
+  $ACLightBlue   = "\x1B[1;34m";
+  $ACLightGreen  = "\x1B[1;32m";
+  $ACLightCyan   = "\x1B[1;36m";
+  $ACLightRed	 = "\x1B[1;31m";
+  $ACLightPurple = "\x1B[1;35m";
+  $ACYellow	 = "\x1B[1;33m";
+  $ACWhite	 = "\x1B[1;37m";
+
+  $ACPlain	 = "\x1B[0m";
+  $ACBold	 = "\x1B[1m";
+  $ACUnderline   = "\x1B[4m";
+  $ACBlink	 = "\x1B[5m";
+  $ACReverse	 = "\x1B[7m";
+
+  $ACClear	 = "\x1B[2J";
+  $ACClearL	 = "\x1B[2K";
 }
